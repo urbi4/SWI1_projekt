03490fc95741c734896271971841161c3450ecc4
DusekM98 <78905048+DusekM98@users.noreply.github.com>
2021-04-08 16:46:10 +0200

    JavaFX přidána

--- LastUpdateProject/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module LastUpdateProject {
-    requires java.sql;
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires javafx.controls;
-
-    opens sample;
-    opens sample.database;
-}
