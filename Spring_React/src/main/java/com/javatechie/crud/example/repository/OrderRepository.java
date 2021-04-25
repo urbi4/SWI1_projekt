0d90728bdfc0239fb167bafe0e20696573fda761
DusekM98 <78905048+DusekM98@users.noreply.github.com>
2021-04-25 14:00:50 +0200

    Deletion

--- Spring_React/src/main/java/com/javatechie/crud/example/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javatechie.crud.example.repository;
-
-
-import com.javatechie.crud.example.entity.OrdersEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<OrdersEntity, Integer> {
-}
